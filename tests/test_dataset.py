11bf2449e271ea323d995faf3fd78b2713f3d46d
jeherr <johnherr@gmail.com>
2020-05-29 13:25:59 -0400

    moving data and tests up a level

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -9,24 +9,24 @@ from tensorchem.dataset.molecule import MoleculeSet
 
 def test_len_MixedDataset():
     mixed_data = MixedDataset()
-    mixed_data.load('tensorchem/tests/h2o.dset')
+    mixed_data.load('tests/h2o.dset')
     assert mixed_data.__len__() == 2
 
 
 def test_getitem_MixedDataset():
     mixed_data = MixedDataset()
-    mixed_data.load('tensorchem/tests/h2o.dset')
+    mixed_data.load('tests/h2o.dset')
     assert list(mixed_data.__getitem__(0).keys()) == ["atomic_numbers", "coordinates", "deg_of_freedom"]
 
 
 def test_save_nofile_MixedDataset():
     mixed_data = MixedDataset()
-    mixed_data.load('tensorchem/tests/h2o.dset')
+    mixed_data.load('tests/h2o.dset')
     with pytest.raises(FileNotFoundError):
         mixed_data.save()
 
 
 def test_MixedDataset_from_mset():
     mset = MoleculeSet()
-    mset.load('tensorchem/tests/h2o.mset')
+    mset.load('tests/h2o.mset')
     assert type(MixedDataset.from_mset(mset)) == tensorchem.dataset.dataset.MixedDataset
